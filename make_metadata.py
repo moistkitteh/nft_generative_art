95fb5d14ad1b11d1d62f3819524256d675f74479
bold <2985057+AboldUSER@users.noreply.github.com>
2021-10-19 10:55:00 -0700

    Update json files to produce LF type

--- make_metadata.py
+++ make_metadata.py
@@ -2,9 +2,9 @@ import os
 
 def main():
 	###YOU NEED TO MANUALLY SET ALL OF THESE####
-	iname = 'test name'					#default filename you used for your images you uploaded to pinata
+	iname = ''					#default filename you used for your files you uploaded to pinata
 	itype = '.png'  				#the file extension you exported with
-	address= 'ipfs/[IPFSHASH]' 		#The IPFS hash to the directory all of your images are hosted at
+	address= 'ipfs://QmXtr1NjrvafSM2NkzBqEwh8E6uCFFDVGv46uCrf1FXMG1' 		#The IPFS hash to the directory all of your images are hosted at
 	mpath = 'metadata/'				#The path to the local folder you want all your metadata files saved at
 	descr = 'test description'			#The description you wanted saved in all of your metadata files
 
@@ -12,7 +12,7 @@ def main():
 	#Reading in image and trait information
 	traits=[]
 	tnames=[]
-	with open('imagetraits.csv') as f:
+	with open('imagetraits.csv','rU') as f:
 		for line in f:
 			temp = []
 			line = line.split(',')
@@ -30,11 +30,11 @@ def main():
 	#Writing out metadata files
 	for x in traits:
 		num = x[0]
-		json=open(mpath+iname+x[0]+'.json','w')
+		json=open(mpath+iname+x[0],'w',newline='\n')
 		json.write('{\n')
 		json.write('\t"name": "'+iname+' #'+num+'",\n')
 		json.write('\t"description": "'+descr+'",\n')
-		json.write('\t"image": "'+address+'/'+num+itype+'",\n')
+		json.write('\t"image": "'+address+'/image'+num+itype+'",\n')
 		json.write('\t"attributes":[\n')
 		z = 0
 		for y in x:
